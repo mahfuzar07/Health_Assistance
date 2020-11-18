8af399971729ef81193830ef18f161c643834266
Md.Mahfuzar Rahman <mahfuzar271@gmail.com>
2020-11-18 21:51:39 +0600

    6th

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,18 @@
     </div>
 </div> --}}
 
+<div class="hero__item set-bg" data-setbg="{{ asset('frontend') }}/img/hero/banner.jpg">
+                        <div class="hero__text">
+                            <span>FRUIT FRESH</span>
+                            <h2>Vegetable <br />100% Organic</h2>
+                            <p>Free Pickup and Delivery Available</p>
+                            <a href="#" class="primary-btn">SHOP NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
  <!-- Categories Section Begin -->
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
+<div class="hero__item set-bg" data-setbg="{{ asset('frontend') }}/img/hero/banner.jpg">
+                        <div class="hero__text">
+                            <span>FRUIT FRESH</span>
+                            <h2>Vegetable <br />100% Organic</h2>
+                            <p>Free Pickup and Delivery Available</p>
+                            <a href="#" class="primary-btn">SHOP NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
  <!-- Categories Section Begin -->
     <section class="categories">
         <div class="container">
