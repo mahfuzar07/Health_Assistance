045db4dabfa889463b076e1dcfd8ebf3c5791fde
Md.Mahfuzar Rahman <58438084+mahfuzar07@users.noreply.github.com>
2021-04-18 15:59:38 +0600

    final

--- resources/views/pages/blood.blade.php
+++ resources/views/pages/blood.blade.php
@@ -2,140 +2,149 @@
 @section('blood') active @endsection
 @section('content')
 
-</div>
-</div>
-</section>
+    </div>
+    </div>
+    </section>
 
 
     <section class="breadcrumb-section set-bg" data-setbg="{{ asset('frontend') }}/img/breadcrumb.jpg">
         <div class="container">
-	            <div class="row">
-	                <div class="col-lg-12 text-center">
-	                    <div class="breadcrumb__text">
-	                        <h2> ব্লাড অনুসন্ধান </h2>
-                            {{-- <h5 class="text-white"><i class="fa fa-heart"></i> আমার রক্তে যদি সহযোগিতা করে মুমূর্ষ রোগীর প্রাণ তাহলে আমি কেন করবোনা স্বেচ্ছায় রক্ত দেন <i class="fa fa-heart"></i></h5> --}}
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2> ব্লাড অনুসন্ধান </h2>
+                        {{-- <h5 class="text-white"><i class="fa fa-heart"></i> আমার রক্তে যদি সহযোগিতা করে মুমূর্ষ রোগীর প্রাণ তাহলে আমি কেন করবোনা স্বেচ্ছায় রক্ত দেন <i class="fa fa-heart"></i></h5> --}}
 
-	                    </div>
-	                </div>
-	            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
 
-        <!-- Blog Section Begin -->
+    <!-- Blog Section Begin -->
     <section class="blog spad">
         <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-5">
                     <div class="blog__sidebar">
 
-                    	<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  <i class="fa fa-hospital-o"></i> ব্লাড এর জন্য পোস্ট করুন
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            <i class="fa fa-hospital-o"></i> ব্লাড এর জন্য পোস্ট করুন
 
-</button>
-<hr>
-
-
+                        </button>
+                        <hr>
 
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header bg-info text-white">
-        <h5 class="modal-title text-center" id="exampleModalLabel"> ব্লাড এর জন্য পোস্ট করুন
-        <p> <i class="fa fa-heart"></i> আপনার প্রয়োজনে আমরা সকলেই আপনার পাশেই আছি <i class="fa fa-heart"></i></p> </h5>
 
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      	<form action="{{ route('store-blood') }}" method="POST">
-         @csrf
-         <div class="row">
-                        <div class="col-lg-12 col-md-6">
-                         <div class="checkout__input">
-                                        <p>রোগীর নাম</p>
-                                        <input type="text" name="full_name" placeholder="রোগীর নাম লিখুন" >
-                                                    @error('full_name')
-                                                    <strong class="text-danger">{{ $message }}</strong>
-                                                    @enderror
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header bg-info text-white">
+                                        <h5 class="modal-title text-center" id="exampleModalLabel"> ব্লাড এর জন্য পোস্ট করুন
+                                            <p> <i class="fa fa-heart"></i> আপনার প্রয়োজনে আমরা সকলেই আপনার পাশেই আছি <i
+                                                    class="fa fa-heart"></i></p>
+                                        </h5>
+
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
                                     </div>
-                            <div class="checkout__input">
-                                <p>হাসপাতালের নাম </p>
-                                <input type="text" name="hospital_name" placeholder="হাসপাতালের নাম লিখুন" >
-                                                    @error('hospital_name')
-                                                    <strong class="text-danger">{{ $message }}</strong>
-                                                    @enderror
+                                    <div class="modal-body">
+                                        <form action="{{ route('store-blood') }}" method="POST">
+                                            @csrf
+                                            <div class="row">
+                                                <div class="col-lg-12 col-md-6">
+                                                    <div class="checkout__input">
+                                                        <p>রোগীর নাম</p>
+                                                        <input type="text" name="full_name" placeholder="রোগীর নাম লিখুন">
+                                                        @error('full_name')
+                                                            <strong class="text-danger">{{ $message }}</strong>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="checkout__input">
+                                                        <p>হাসপাতালের নাম </p>
+                                                        <input type="text" name="hospital_name"
+                                                            placeholder="হাসপাতালের নাম লিখুন">
+                                                        @error('hospital_name')
+                                                            <strong class="text-danger">{{ $message }}</strong>
+                                                        @enderror
 
-                            </div>
-                            <div class="checkout__input">
-                                <p>ঠিকানা</p>
-                                <input type="text" name="address" placeholder="সড়ক নং , উপজেলা, জেলা " class="checkout__input__add">
+                                                    </div>
+                                                    <div class="checkout__input">
+                                                        <p>ঠিকানা</p>
+                                                        <input type="text" name="address"
+                                                            placeholder="সড়ক নং , উপজেলা, জেলা "
+                                                            class="checkout__input__add">
 
-                                     @error('address')
-                                     <strong class="text-danger">{{ $message }}</strong>
-                                      @enderror
-                             </div>
-                             <div class="row">
-                                <div class="col-lg-9">
-                                    <div class="checkout__input">
-                                        <p>মোবাইল নং</p>
-                                        <input type="text" name="phone" placeholder="মোবাইল নং" >
-                                                    @error('phone')
-                                                    <strong class="text-danger">{{ $message }}</strong>
-                                                    @enderror
+                                                        @error('address')
+                                                            <strong class="text-danger">{{ $message }}</strong>
+                                                        @enderror
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-lg-9">
+                                                            <div class="checkout__input">
+                                                                <p>মোবাইল নং</p>
+                                                                <input type="text" name="phone" placeholder="মোবাইল নং">
+                                                                @error('phone')
+                                                                    <strong class="text-danger">{{ $message }}</strong>
+                                                                @enderror
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-lg-3">
+                                                            <div class="checkout__input">
+                                                                <p>ব্লাড গ্রুপ </p>
+                                                                <input type="text" name="blood_group" placeholder="">
+                                                                @error('blood_group')
+                                                                    <strong class="text-danger">{{ $message }}</strong>
+                                                                @enderror
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="checkout__input">
+                                                        <p>রোগীর অতিরিক্ত বিবরণ</p>
+                                                        <input type="text" name="note"
+                                                            placeholder="রোগীর অতিরিক্ত কিছু বিবরণ থাকলে এখানে লিখুন">
+
+
+                                                        @error('note')
+                                                            <strong class="text-danger">{{ $message }}</strong>
+                                                        @enderror
+                                                    </div>
+
+                                                </div>
+
+
+                                            </div>
+                                            <button type="submit"
+                                                class="btn btn-success waves-effect waves-light m-l-10 pull-right"> পোস্ট
+                                                করুন </button>
+
+                                        </form>
+
                                     </div>
+
                                 </div>
-                                <div class="col-lg-3">
-                                    <div class="checkout__input">
-                                      <p>ব্লাড গ্রুপ </p>
-                                        <input type="text" name="blood_group" placeholder="" >
-                                                    @error('blood_group')
-                                                    <strong class="text-danger">{{ $message }}</strong>
-                                                    @enderror
-                                    </div>
-                                </div>
                             </div>
-                            <div class="checkout__input">
-                                <p>রোগীর অতিরিক্ত বিবরণ</p>
-                                <input type="text" name="note"
-                                    placeholder="রোগীর অতিরিক্ত কিছু বিবরণ থাকলে এখানে লিখুন">
-
-
-                                                    @error('note')
-                                                    <strong class="text-danger">{{ $message }}</strong>
-                                                    @enderror
-                            </div>
-
                         </div>
 
 
-                    </div>
-                    <button type="submit" class="btn btn-success waves-effect waves-light m-l-10 pull-right"> পোস্ট  করুন  </button>
-
-                </form>
-
-      </div>
-
-    </div>
-  </div>
-</div>
-
-
-<div class="blog__sidebar__item">
-     <div class=" col-lg-10 form-group">
-    <a href="{{ route('user.blood') }}"><button class="btn btn-primary"> সকল রক্ত দাতা </button></a>
-                 </div>
+                        <div class="blog__sidebar__item">
+                            <div class=" col-lg-10 form-group">
+                                <a href="{{ route('user.blood') }}"><button class="btn btn-primary"> সকল রক্ত দাতা
+                                    </button></a>
+                            </div>
 
 
 
 
 
-</div>
+                        </div>
 
 
 
@@ -150,25 +159,26 @@
 
                     </div>
 
-                        <div class="row">
+                    <div class="row">
 
-                            @foreach ($bloods as $row)
+                        @foreach ($bloods as $row)
                             <div class="col-lg-6 col-md-6 col-sm-6">
 
                                 <div class="blog__item">
                                     <div class="blog__item__text">
-                                            <h5>রোগীর নাম : {{ $row-> full_name  }}</h5>
-                                            <h6>হাসপাতাল : {{ $row->  hospital_name }}</h6>
-                                            <h6>রক্তের গ্ৰুপ: <b>{{ $row-> blood_group  }}</b></h6>
-                                            <h6>যোগাযোগের ঠিকানা:  {{ $row-> address  }}</h6>
+                                        <h5>রোগীর নাম : {{ $row->full_name }}</h5>
+                                        <h6>হাসপাতাল : {{ $row->hospital_name }}</h6>
+                                        <h6>রক্তের গ্ৰুপ: <b>{{ $row->blood_group }}</b></h6>
+                                        <h6>যোগাযোগের ঠিকানা: {{ $row->address }}</h6>
 
-                                            <h6>মোবাইল নং: <b>{{ $row->  phone }}</b> </h6>
-                                            <h6> রোগীর অতিরিক্ত বিবরণ:{{ $row->  note }}</h6>
-                                            <h6> পোস্ট দাতা:{{ $row->join-> name}}</h6>
+                                        <h6>মোবাইল নং: <b>{{ $row->phone }}</b> </h6>
+                                        <h6> রোগীর অতিরিক্ত বিবরণ:{{ $row->note }}</h6>
+                                        <h6> পোস্ট দাতা:{{ $row->join->name }}</h6>
 
                                         <ul>
 
-                                        <li><i class="fa fa-calendar-o"></i> {{ $row-> created_at->diffForHumans()  }}</li>
+                                            <li><i class="fa fa-calendar-o"></i>
+                                                {{ $row->created_at->diffForHumans() }}</li>
 
 
                                         </ul>
@@ -176,19 +186,19 @@
                                     </div>
                                 </div>
                             </div>
-                            @endforeach
+                        @endforeach
 
 
-
-                        </div>
-
-                       <div>
-                      {{ $bloods->links() }}
 
                     </div>
 
+                    <div>
+                        {{ $bloods->links() }}
+
+                    </div>
+
+                </div>
             </div>
-        </div>
     </section>
     <!-- Blog Section End -->
 
--- resources/views/user/uconview.blade.php
+++ resources/views/user/uconview.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.frontend_layouts')
 @section('content')
 
-</div>
-</div>
-</section>
+    </div>
+    </div>
+    </section>
 
 
     <section class="breadcrumb-section set-bg" data-setbg="{{ asset('frontend') }}/img/breadcrumb.jpg">
         <div class="container">
-	            <div class="row">
-	                <div class="col-lg-12 text-center">
-	                    <div class="breadcrumb__text">
-	                        <h2> স্বাস্থ্য  পরামর্শকগণ </h2>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2> স্বাস্থ্য পরামর্শকগণ </h2>
 
-	                    </div>
-	                </div>
-	            </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
@@ -24,78 +24,98 @@
     <section class="product spad">
         <div class="container">
             <div class="row">
-               <div class="col-lg-3 col-md-5" {{-- style="margin-top: 6%;" --}}>
-                <div><h4>প্রোফাইল মেন্যু </h4></div>
-                  <hr>
+                <div class="col-lg-3 col-md-5" {{-- style="margin-top: 6%;" --}}>
+                    <div>
+                        <h4>প্রোফাইল মেন্যু </h4>
+                    </div>
+                    <hr>
 
-                      <div class="list-group">
-  <a href="{{ route('user.profile') }}" class="list-group-item list-group-item-action "><i class="fa fa-address-card" aria-hidden="true"></i>
-    ড্যাশবোর্ড
-  </a>
-  <a href="{{ route('user.orders') }}" class="list-group-item list-group-item-action"><i class="fa fa-truck" aria-hidden="true"></i> অর্ডার তথ্য বিবরণ</a>
-  
-  <a href="{{ route('user.consultant') }}" class="list-group-item list-group-item-action active"><i class="fa fa-user-md" aria-hidden="true"></i> স্বাস্থ্য পরামর্শদাতা</a>
-  
-  <a href="{{ route('blood.page') }}" class="list-group-item list-group-item-action"><i class="fa fa-hospital-o" aria-hidden="true"></i> ব্লাড অনুসন্ধান</a>
- 
-</div>
-   </div>
+                    <div class="list-group">
+                        <a href="{{ route('user.profile') }}" class="list-group-item list-group-item-action "><i
+                                class="fa fa-address-card" aria-hidden="true"></i>
+                            ড্যাশবোর্ড
+                        </a>
+                        <a href="{{ route('user.orders') }}" class="list-group-item list-group-item-action"><i
+                                class="fa fa-truck" aria-hidden="true"></i> অর্ডার তথ্য বিবরণ</a>
+
+                        <a href="{{ route('user.consultant') }}" class="list-group-item list-group-item-action active"><i
+                                class="fa fa-user-md" aria-hidden="true"></i> স্বাস্থ্য পরামর্শদাতা</a>
+
+                        <a href="{{ route('blood.page') }}" class="list-group-item list-group-item-action">
+                            <i class="fa fa-hospital-o" aria-hidden="true">
+                            </i> ব্লাড অনুসন্ধান
+                        </a>
+
+                    </div>
+                </div>
 
 
                 <div class="col-lg-9 col-md-7">
-                  <div><h4 class="text-center">স্বাস্থ্য  পরামর্শকগণের তথ্য বিবরণ</h4></div>
-                  <hr>
-                   @php
-                            $consultants = App\Consultant::where('status',0)->latest()-> get();
-                        @endphp
+                    <div>
+                        <h4 class="text-center">স্বাস্থ্য পরামর্শকগণের তথ্য বিবরণ</h4>
+                    </div>
+                    <hr>
+                    @php
+                        $consultants = App\Consultant::where('status', 0)
+                            ->latest()
+                            ->get();
+                    @endphp
 
 
-                  <div class="row">
-                    @foreach ($consultants as $row)
-                  <div class="col-lg-3 ">
+                    <div class="row">
+                        @foreach ($consultants as $row)
+                            <div class="col-lg-3 ">
 
-                  <div class="card" style="width: 13rem;">
-                <img src="{{asset('uploads/documents/consultant/'. $row-> avatar) }}"class="card-img-top" style=" height: 10rem;">
+                                <div class="card" style="width: 13rem;">
+                                    <img src="{{ asset('uploads/documents/consultant/' . $row->avatar) }}"
+                                        class="card-img-top" style=" height: 10rem;">
 
-                <h6 class="text-center badge badge-primary"><i class="fa fa-user-md"></i> {{ $row-> name }} </h6>
+                                    <h6 class="text-center badge badge-primary"><i class="fa fa-user-md"></i>
+                                        {{ $row->name }} </h6>
 
-                <div class="card-body">
-                  <h6 class=""> {{ $row-> profession }} </h6>
-                  <h6 class=""> {{ $row-> institute }} </h6>
-                  <h6 class="text-primary"><b> {{ $row-> specialist }}</b> </h6>
-                  <p class=""><i class="fa fa-phone"></i> {{ $row-> phone_number}}</p>
-                  <a href="{{ route('chatcon',$row->id.'-'.str_slug($row->name)) }}"><button  type="button" class="btn btn-primary btn-sm"><i class="fa fa-envelope"></i> বার্তা পাঠান  </button></a>
-
-                 {{--  <a href=""><button  type="button" class="btn btn-primary btn-sm"><i class="fa fa-phone"></i></button></a>
-
-                  <a href=""><button  type="button" class="btn btn-primary btn-sm"><i class="fa fa-video-camera"></i></button></a> --}}
-                </div>
-
-              </div>
-              <hr>
-              </div>
-
-              @endforeach
+                                    <div class="card-body">
+                                        <h6 class=""> {{ $row->profession }} </h6>
+                                        <h6 class=""> {{ $row->institute }} </h6>
+                                        <h6 class="text-primary"><b> {{ $row->specialist }}</b> </h6>
+                                        <p class=""><i class="fa fa-phone"></i> {{ $row->phone_number }}</p>
 
 
-
-              </div>
-
-
-
-
+                                        <a href="{{ route('chatcon', $row->id . '-' . str_slug($row->name)) }}"><button
+                                                type="button" class="btn btn-primary btn-sm"><i class="fa fa-envelope"></i>
+                                                Message
+                                            </button></a>
 
 
+                                        <a href=""><button type="button" class="btn btn-primary btn-sm"><i
+                                                    class="fa fa-video-camera"></i> Calling </button></a>
 
+                                    </div>
 
+                                </div>
+                                <hr>
+                            </div>
 
+                        @endforeach
 
 
 
                     </div>
 
+
+
+
+
+
+
+
+
+
+
+
                 </div>
+
             </div>
+        </div>
         </div>
     </section>
     <!-- Product Section End -->
